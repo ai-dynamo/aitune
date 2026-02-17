d557445f3f5824b7a154fbcf0023f09482d03fd0
Piotr Bazan <pbazan@nvidia.com>
2026-02-17 05:26:04 -0800

    chore: "Update copyright year from 2025 to 2025-2026"

--- aitune/torch/dataloader.py
+++ aitune/torch/dataloader.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025-2026, NVIDIA CORPORATION. All rights reserved.
 # Copyright 2020 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- examples/common/aitune_examples_common/batching.py
+++ examples/common/aitune_examples_common/batching.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2025-2026, NVIDIA CORPORATION. All rights reserved.
 # Copyright 2023 Ray Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
