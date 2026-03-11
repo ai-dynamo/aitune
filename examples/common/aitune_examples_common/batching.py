b7033748f4b907048223a5c55b42c21e982509c6
Jakub Kisel <jkisel@nvidia.com>
2026-03-11 03:37:26 -0700

    Update license headers to short format

--- examples/common/aitune_examples_common/batching.py
+++ examples/common/aitune_examples_common/batching.py
@@ -1,17 +1,6 @@
-# Copyright (c) 2025-2026, NVIDIA CORPORATION. All rights reserved.
-# Copyright 2023 Ray Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright 2023 Ray Authors
+# SPDX-License-Identifier: Apache-2.0
 """Dynamic batching decorator for async endpoints in AI Dynamo."""
 
 import asyncio
